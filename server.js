59d5ce41d274922ac89995a6cad4af3573f77269
Jathavedas <jathavedaskavikumar@gmail.com>
2026-05-26 12:40:17 +0530

    Secure CORS configuration

--- server.js
+++ server.js
@@ -17,7 +17,17 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+const allowedOrigins = ['http://localhost:5173', 'https://akash-omega-seven.vercel.app'];
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
